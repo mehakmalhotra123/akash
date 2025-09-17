be1b30433e30d1ccf54c2a5ea0f9ae37fb54fba4
sandeep.singh <sandeep.singh@brightzone.us>
2025-09-17 13:42:13 +0530

    updated app.js. Added -V2 | Sandee Singh

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Health endpoint
 app.get("/health", (req, res) => {
-  res.json({ status: "API is healthy 🚀" });
+  res.json({ status: "API is healthy 🚀! - v2" });
 });
 
 // Routes
